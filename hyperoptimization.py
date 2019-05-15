bcad6060a91298f2f0a35b67302746b84bf11e83
alfaluis <luis.alfaro@komatsu.cl>
2019-05-15 08:52:19 -0400

    delete unneeded libraries

--- hyperoptimization.py
+++ hyperoptimization.py
@@ -1,12 +1,10 @@
 from hyperopt import tpe, Trials, STATUS_OK, fmin
 import pandas
-from testing_xgb_early_stopping import get_num_estimators_xgb
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, LinearSVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_auc_score
 import numpy as np
 import pandas as pd
 import xgboost as xgb
