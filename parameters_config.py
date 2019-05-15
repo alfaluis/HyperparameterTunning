0e3068786fd2cd9f1997758704e1be4c51da58d8
alfaluis <luis.alfaro@komatsu.cl>
2019-05-15 01:07:30 -0400

    Add support for LR and RF. Rename names

--- parameters_config.py
+++ parameters_config.py
@@ -1,58 +1,57 @@
 from hyperopt import hp
+import numpy as np
 
 # Parameters configuration for KNN
-knn_parameters = {'n_neighbors': hp.choice('n_neighbors', range(1, 100))}
+knn_params = {'n_neighbors': hp.choice('n_neighbors', range(1, 100))}
 
 # parameters configuration for xgb
-xgb_parameters = {'learning_rate': hp.loguniform('learning_rate', 0.001, 3),
-                  'gamma': hp.loguniform('gamma', 0.1, 2),
-                  'max_depth': hp.randint('max_depth', 20),
-                  'min_child_weight': hp.randint('min_child_weight', 10),
-                  'subsample': hp.uniform('subsample', 0.1, 1),
-                  'colsample_bytree': hp.uniform('colsample_bytree', 0.3, 1),
-                  'colsample_bylevel': hp.uniform('colsample_bylevel', 0.3, 1),
-                  'colsample_bynode': hp.uniform('colsample_bynode', 0.3, 1),
-                  'scale_pos_weight': hp.choice('scale_pos_weight', [1, 'balanced']),
-                  'max_delta_step': hp.randint('max_delta_step', 10)
-                  # 'booster': hp.choice('booster', ['gbtree', 'gblinear', 'dart']),
-                  }
+xgb_params = {'learning_rate': hp.loguniform('learning_rate', 0.001, 3),
+              'gamma': hp.loguniform('gamma', 0.1, 2),
+              'max_depth': hp.randint('max_depth', 20),
+              'min_child_weight': hp.randint('min_child_weight', 10),
+              'subsample': hp.uniform('subsample', 0.1, 1),
+              'colsample_bytree': hp.uniform('colsample_bytree', 0.3, 1),
+              'colsample_bylevel': hp.uniform('colsample_bylevel', 0.3, 1),
+              'colsample_bynode': hp.uniform('colsample_bynode', 0.3, 1),
+              'scale_pos_weight': hp.choice('scale_pos_weight', [1, 'balanced']),
+              'max_delta_step': hp.randint('max_delta_step', 10)
+              # 'booster': hp.choice('booster', ['gbtree', 'gblinear', 'dart']),
+              }
 
 # parameters configuration for svm
-svm_parameters = {
-    'param': hp.choice('param', [{'kernel': 'linear',
-                                  },
-                                 {'kernel': 'rbf',
-                                  'gamma':  hp.loguniform('gamma_rbf', 0.1, 3),
-                                  },
-                                 {'kernel': 'poly',
-                                  'degree': hp.quniform('degree', 2, 10, 2),
-                                  'gamma':  hp.uniform('gamma_poly', 0.1, 3),
-                                  'coef0': hp.uniform('coef0', 0, 1)
-                                  }
-                                 ]),
-    'C': hp.uniform('C', 0.01, 100),
-    'class_weight': hp.choice('class_weight', ['balanced', 1])
-}
+svm_params = {'param': hp.choice('param', [{'kernel': 'linear',
+                                            },
+                                           {'kernel': 'rbf',
+                                            'gamma':  hp.loguniform('gamma_rbf', 0.1, 3)
+                                            },
+                                           {'kernel': 'poly',
+                                            'degree': hp.quniform('degree', 2, 10, 2),
+                                            'gamma':  hp.uniform('gamma_poly', 0.1, 3),
+                                            'coef0': hp.uniform('coef0', 0, 1)
+                                            }
+                                           ]),
+              'C': hp.uniform('C', 0.01, 100),
+              'class_weight': hp.choice('class_weight', ['balanced', 1])
+              }
 
-lr_params = {'penalty': hp.choice('penalty', ['l1', 'l2']),
-             'C': hp.uniform('C', 0.01, 100),
+lr_params = {'param': hp.choice('param', [{'penalty': 'l1',
+                                           'solver': hp.choice('solver-l1', ['liblinear', 'saga']),
+                                           },
+                                          {'penalty': 'l2',
+                                           'solver': hp.choice('solver-l2', ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'])
+                                           }
+                                          ]),
+             'C': hp.loguniform('C', np.log(0.01), np.log(100)),
              'class_weight': hp.choice('class_weight', ['balanced', 1]),
-             'solver': hp.choice('solver', ['lbfgs', 'liblinear', 'newton-cg'])
+             'max_iter': 500 + hp.randint('max_iter', 1000)
              }
-rf_params = {}
-"""
-hp.choice('classifier',[
-        {'model': KNeighborsClassifier,
-        'param': {'n_neighbors': hp.choice('n_neighbors',range(3,11)),
-                  'algorithm':hp.choice('algorithm',['ball_tree','kd_tree']),
-                  'leaf_size':hp.choice('leaf_size',range(1,50)),
-                  'metric':hp.choice('metric', ["euclidean","manhattan", "chebyshev","minkowski"])}
-        },
-        {'model': SVC,
-        'param':{'C':hp.lognormal('C',0,1),
-        'kernel':hp.choice('kernel',['rbf','poly','rbf','sigmoid']),
-        'degree':hp.choice('degree',range(1,15)),
-        'gamma':hp.uniform('gamma',0.001,10000)}
-        }
-        ])
-"""
+
+# Parameters for Random Forest
+rf_params = {'n_estimators': 50 + hp.randint('n_estimators', 1950),
+             'max_features': hp.choice('max_features', ['auto', 'sqrt']),
+             'criterion': hp.choice('criterion', ['gini', 'entropy']),
+             'class_weight': hp.choice('class_weight', ['balanced', 'balanced_subsample', None]),
+             'bootstrap': hp.choice('bootstrap', [True, False]),
+             'min_samples_split': 2 + hp.randint('min_samples_split', 10),
+             'min_samples_leaf': 1 + hp.randint('min_samples_leaf', 10),
+             'max_depth': 1 + hp.randint('max_depth', 10)}
